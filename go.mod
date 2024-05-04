d276eac842c4081b6eb11888ee3dc3de291a388f
Shreyank031 <shreyankchandu@gmail.com>
2024-05-04 09:13:02 +0530

    Add get, create, delete, update movies functions

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Shreyank031/Go_Movie_CRUD
 
 go 1.22.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -23,8 +23,71 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	//set the headers
+	w.Header().Set("Content-Type", "application/json")
+	//encode the json and send a responce to the user
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovieById(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = params["id"]
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movies)
+
+}
+
 func main() {
-	movies = append(movies, Movie{ID: "1", Isbn: "313288", Title: "KGF", Director: &Director{FirstName: "Jack", LastName: "Sun"}})
+	movies = append(movies, Movie{ID: "1", Isbn: "313288", Title: "KGF", Director: &Director{FirstName: "Prashanth", LastName: "Neel"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "92214", Title: "12th Fail", Director: &Director{FirstName: "Vinod", LastName: "Chopra"}})
 
 	r := mux.NewRouter()
